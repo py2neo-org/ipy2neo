30513bd3a2caece81cab2ebca20e2ee004e736c6
Nigel Small <nasmall@pm.me>
2021-06-19 23:48:00 +0100

    Imported argument decorator locally

--- ipy2neo/__main__.py
+++ ipy2neo/__main__.py
@@ -21,7 +21,35 @@ from __future__ import print_function
 from argparse import ArgumentParser
 from inspect import getdoc
 
-from py2neo.compat import argument
+
+def argument(*args, **kwargs):
+    """ Decorator for specifying argparse arguments attached to a
+    function.
+
+    ::
+
+        @argument("-v", "--verbose", action="count", default=0,
+                  help="Increase verbosity.")
+        def foo(verbose):
+            pass
+
+    """
+
+    def f__(f):
+        def f_(*a, **kw):
+            return f(*a, **kw)
+
+        f_.__name__ = f.__name__
+        f_.__doc__ = f.__doc__
+        f_.__dict__.update(f.__dict__)
+        if hasattr(f, "arguments"):
+            f_.arguments = f.arguments
+        else:
+            f_.arguments = []
+        f_.arguments.insert(0, (args, kwargs))
+        return f_
+
+    return f__
 
 
 @argument("-u", "--uri",
